f94621d64b6b8d40dbadb533bc783abf5b7935eb
victorclee <victorchristopherlee@gmail.com>
2018-02-20 20:40:13 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-### This is an Rails app built to try different customizable generator features
+## This is an Rails app built to try different customizable generator features
+
+### In `config/application.rb`
+
+```ruby
+class Application < Rails::Application
+  config.generators do |g|
+    g.orm             :active_record
+    g.template_engine :erb
+    g.test_framework  :test_unit, fixture: false
+    g.stylesheets     false
+    g.javascripts     false
+  end
+end
+```
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220025722) do
+ActiveRecord::Schema.define(version: 20180221023240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
